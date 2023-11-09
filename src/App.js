7812e5f60a9698d1043a7d04ed08086391610ee9
mainul96 <110577655+mainul96@users.noreply.github.com>
2023-11-09 22:20:55 +0530

    somechanges

--- src/App.js
+++ src/App.js
@@ -6,42 +6,44 @@ import Home from './components/Home/Home';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
 import Orders from './components/Orders/Orders';
-import PraviteRoutes from './routes/PraviteRoutes';
+import PrivateRoutes from './routes/PrivateRoutes';
+
+
 
 
 const router = createBrowserRouter([
-{
-  path:'/',
-  element:<Main></Main>,
-  children:[
-    {
-      path:'/',
-      element:<Home></Home>
-    },
-    {
-      path:'/home',
-      element:<Home></Home>
-    },
-    {
-      path:'/orders',
-      element:<PraviteRoutes><Orders></Orders></PraviteRoutes>
-    },
-    {
-      path:'/register',
-      element:<Register></Register>
-    },
-    {
-      path:'/login',
-      element:<Login></Login>
-    }
-  ]
-}
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/home',
+        element: <Home></Home>
+      },
+      {
+        path: '/orders',
+        element: <PrivateRoutes><Orders></Orders></PrivateRoutes>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      }
+    ]
+  }
 ])
 
 function App() {
   return (
     <div className="App">
-     <RouterProvider router={router}></RouterProvider>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
